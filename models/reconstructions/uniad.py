aad3be63cb525ec5da7eee3d6c11b0a65942a218
NguyenMinhTri <chinjsu130205@gmail.com>
2025-11-22 22:43:09 +0700

    20%maskmemtrain, sig, Refmem, neighbor no print debug

--- models/reconstructions/uniad.py
+++ models/reconstructions/uniad.py
@@ -474,7 +474,7 @@ class UniADMemory(nn.Module):
 
         # Project input features
         feature_tokens = self.input_proj(feature_tokens)  # (H x W) x B x C
-        # feature_tokens = F.layer_norm(feature_tokens, feature_tokens.shape[-1:])
+        feature_tokens = F.layer_norm(feature_tokens, feature_tokens.shape[-1:])
         # x_min, x_max = feature_tokens.min(), feature_tokens.max()
         # feature_tokens = (feature_tokens - x_min) / (x_max - x_min + 1e-6)
         # x_min, x_max = feature_tokens.min(), feature_tokens.max()
@@ -561,7 +561,7 @@ class UniADMemory(nn.Module):
         
         # Project back to original dimension
         feature_rec_tokens = self.output_proj(decoded_tokens)  # (H x W) x B x C
-        # feature_rec_tokens = torch.sigmoid(feature_rec_tokens)
+        feature_rec_tokens = torch.sigmoid(feature_rec_tokens)
         # feature_rec_tokens = F.layer_norm(feature_rec_tokens, feature_rec_tokens.shape[-1:])
         # x_min, x_max = feature_rec_tokens.min(), feature_rec_tokens.max()
         # feature_rec_tokens = 2 * (feature_rec_tokens - x_min) / (x_max - x_min + 1e-6) - 1
@@ -587,7 +587,7 @@ class UniADMemory(nn.Module):
                 np.save(os.path.join(save_dir, filename_ + ".npy"), feature_rec_np)
 
         # Compute prediction (reconstruction error)
-        # feature_align = torch.sigmoid(feature_align) 
+        feature_align = torch.sigmoid(feature_align) 
         # feature_align = F.layer_norm(feature_align, feature_align.shape[1:])
         # x_min, x_max = feature_align.min(), feature_align.max()
         # feature_align = 2 * (feature_align - x_min) / (x_max - x_min + 1e-6) - 1
