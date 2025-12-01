140e83be9462205d51e9328e4d11ab78bc7b4387
NguyenMinhTri <chinjsu130205@gmail.com>
2025-12-01 12:41:36 +0700

    add multistepLR

--- utils/lr_helper.py
+++ utils/lr_helper.py
@@ -4,5 +4,7 @@ import torch
 def get_scheduler(optimizer, config):
     if config.type == "StepLR":
         return torch.optim.lr_scheduler.StepLR(optimizer, **config.kwargs)
+    elif config.type == "MultiStepLR":
+        return torch.optim.lr_scheduler.MultiStepLR(optimizer, **config.kwargs)
     else:
         raise NotImplementedError
